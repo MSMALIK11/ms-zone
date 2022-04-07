d8feccf3d96360517a0a011ab8a4282d83c55874
MR. SHOAIB <msm76441@gmail.com>
2022-04-07 12:29:58 +0530

    install materiala ui library

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders lets build e-commere website", () => {
+test("render lets build e-commere website", () => {
   render(<App />);
   const linkElement = screen.getByText(/lets build e-commere website/i);
   expect(linkElement).toBeInTheDocument();
